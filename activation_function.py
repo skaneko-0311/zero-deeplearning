7a1426aa55ecc7e1fd98a04ca8a84c45ba266f8b
skaneko-0311 <skaneko.0311@gmail.com>
2019-05-28 06:08:07 -0700

    Add RELU for activation function

--- activation_function.py
+++ activation_function.py
@@ -7,3 +7,7 @@ def step_function(x):
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
--- test/test_activation_function.py
+++ test/test_activation_function.py
@@ -14,6 +14,12 @@ class TestActivationFunction(unittest.TestCase):
         self.assertEqual(af.sigmoid(0),  0.5)
         self.assertEqual(af.sigmoid(1),  0.7310585786300049)
 
+    def test_relu(self):
+        self.assertEqual(af.relu(-1), 0)
+        self.assertEqual(af.relu(0),  0)
+        self.assertEqual(af.relu(1),  1)
+        self.assertEqual(af.relu(5),  5)
+
 
 if __name__ == '__main__':
     unittest.main()
