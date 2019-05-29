a46d9689e04e39d8b30e0d920f3b4282df14d1dd
skaneko-0311 <skaneko.0311@gmail.com>
2019-05-29 05:48:01 -0700

    Add softmax function

--- activation_function.py
+++ activation_function.py
@@ -15,3 +15,10 @@ def relu(x):
 
 def identity_function(x):
     return x
+
+
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
--- test/test_activation_function.py
+++ test/test_activation_function.py
@@ -1,4 +1,5 @@
 import unittest
+import numpy as np
 import activation_function as af
 
 
@@ -25,6 +26,14 @@ class TestActivationFunction(unittest.TestCase):
         self.assertEqual(af.identity_function(0),   0)
         self.assertEqual(af.identity_function(1),   1)
         self.assertEqual(af.identity_function(5),   5)
+
+    def test_softmax(self):
+        a = np.array([0.3, 2.9, 4.0])
+        np.testing.assert_allclose(af.softmax(a),
+                                   [0.01821127,
+                                    0.24519181,
+                                    0.73659691],
+                                   rtol=1e-6, atol=0)
 
 
 if __name__ == '__main__':
